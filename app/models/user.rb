62cca199fa207f6b8b79555e5e9d915dc11f2699
Eliza Khachatryan <elijbet@gmail.com>
2017-03-12 22:13:53 -0700

    controller namespace and routing separate admin site layout from front-end layout

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+	has_many :posts
+	
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :password, presence: true
+	validates :password, confirmation: true
+
+end
